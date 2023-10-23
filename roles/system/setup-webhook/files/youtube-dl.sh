ea4427977832dae8214548f345aaa849aa0a82fe
DictumMortuum <theio.vrefos@gmail.com>
2023-10-23 17:59:23 +0300

    [system/setup-webhook] removed download.sh, added youtube-dl.sh

---
+++ roles/system/setup-webhook/files/youtube-dl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH=${1}
+URL=${2}
+OWNER=${3}
+GROUP=${4}
+
+cd "${PATH}"
+echo "$PATH $URL $OWNER $GROUP"
+/bin/sudo /usr/local/bin/docker run --rm -i \
+  -e PGID=$(/bin/id -g ${GROUP}) \
+  -e PUID=$(/bin/id -u ${OWNER}) \
+  -v "${PATH}":/workdir:rw \
+  mikenye/youtube-dl \
+  ${URL}
